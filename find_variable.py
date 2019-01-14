7dd7800fc301a6685fdc430b60b2dde683a46f80
HeungBaeLEE <hb06191@gmail.com>
2019-01-14 14:37:51 +0900

    Update find_variable.py

--- find_variable.py
+++ find_variable.py
@@ -12,9 +12,9 @@ def fine_variable(alpha):
 
     # 기사 데이터 프레임 로드
     # os 모듈을 이용해서 파일의 경로를 가져옴
-    path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.dirname(os.path.realpath("article_2016-06-01.plk"))
     print(path)
-    article_df = pd.read_pickle("{}article_2016-06-01.plk".format(path))
+    article_df = pd.read_pickle("{}/article_2016-06-01.plk".format(path))
 
     # 테스트 데이터와 트레인 데이터 분리
     X_train, X_test, y_train, y_test = train_test_split(article_df.content, article_df.category, test_size=0.1, random_state=1)
